6734534eb1c4197098590c9cc5360277c8e74559
Amarikwa <amarikwa188@gmail.com>
2024-07-30 21:50:16 +0100

    basic layout and deletion functionality

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+
+app: Flask  = Flask(__name__)
+
+urls: list[str] = ["www.example.com",
+                   "www.anotherexamplefortesting.gov",
+                   "www.evenmore.com.uk",
+                   "www.example.com/long-extra-details-details",
+                   "www.thisoneisonlineforsure.com",
+                   "www.example.com",
+                   "www.anotherexamplefortesting.gov",
+                   "www.evenmore.com.uk",
+                   "www.example.com/long-extra-details-details-detailsdetailsdetails",
+                   "www.thisoneisonlineforsure.com"]
+
+# urls = []
+
+
+@app.route("/", methods=["POST", "GET"])
+def index() -> str:
+    if request.method == "POST":
+        print("\npost\n")
+        choice: str = list(request.form.keys())[0]
+        url: str = request.form[choice]
+        print(choice)
+        print(url)
+
+        match choice:
+            case "C":
+                pass
+            case "D":
+                urls.remove(url)
+            case "A":
+                pass
+            
+        return redirect(url_for("index"))
+    else:
+        print("\nget\n")
+        return render_template("index.html", urls=urls)
+    
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
