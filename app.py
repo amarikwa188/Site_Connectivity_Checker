154cb566412a401dd4179d29b393674e892ae004
Amarikwa <amarikwa188@gmail.com>
2024-08-02 20:28:48 +0100

    redone basic layout

--- app.py
+++ app.py
@@ -3,10 +3,13 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app: Flask  = Flask(__name__)
 
+urls: list[str] = ["example1.com", "example2.com", "example3.com",
+                   "example4.com", "example5.com", "example6.com"]
+
 
 @app.route("/", methods=["POST", "GET"])
 def index() -> str:
-    pass
+    return render_template("index.html", urls=urls)
     
 
 if __name__ == "__main__":
