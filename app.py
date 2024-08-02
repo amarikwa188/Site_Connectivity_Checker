ea7415bf61ed0904464dd65a0f0afcdd69bec9ce
Amarikwa <amarikwa188@gmail.com>
2024-08-02 18:49:15 +0100

    starting from scratch with new implementation

--- app.py
+++ app.py
@@ -3,44 +3,11 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app: Flask  = Flask(__name__)
 
-urls: list[str] = ["www.example.com",
-                   "www.anotherexamplefortesting.gov",
-                   "www.evenmore.com.uk",
-                   "www.example.com/long-extra-details-details",
-                   "www.thisoneisonlineforsure.com",
-                   "www.example.com",
-                   "www.anotherexamplefortesting.gov",
-                   "www.evenmore.com.uk",
-                   "www.example.com/long-extra-details-details-detailsdetailsdetails",
-                   "www.thisoneisonlineforsure.com"]
-
-# urls = []
-
 
 @app.route("/", methods=["POST", "GET"])
 def index() -> str:
-    if request.method == "POST":
-        print("\npost\n")
-        choice: str = list(request.form.keys())[0]
-        url: str = request.form[choice]
-        print(choice)
-        print(url)
-
-        if choice == "C":
-            pass
-        elif choice == "D":
-            urls.remove(url)
-        else:
-            if url not in urls:
-                urls.append(url)
-            
-            
-        return redirect(url_for("index"))
-    else:
-        print("\nget\n")
-        return render_template("index.html", urls=urls)
+    pass
     
-
 
 if __name__ == "__main__":
     app.run(debug=True)
